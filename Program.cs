db0d5c77e4a3f89183072ecdcb03b538f35dcf94
Sidhant Malik <96983080+sidhant2094@users.noreply.github.com>
2025-07-06 18:09:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int s = 0;
+int m = 0;
+
+for (int i = 1; i <= 100; i++)
+{
+    s = i % 3;
+    m = i % 5;
+    if (s == 0 && m != 0)
+    {
+        Console.WriteLine($"{i} - Fizz");
+    }
+    else if (s != 0 && m == 0)
+    {
+        Console.WriteLine($"{i} - Buzz");
+    }
+    else if (s == 0 && m == 0)
+    {
+        Console.WriteLine($"{i} - FizzBuzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
